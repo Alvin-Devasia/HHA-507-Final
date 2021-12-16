0755aecda8facb1ba32a2f8885c9c9d25a5ff2d8
Alvin <alvin.devasia@stonybrook.edu>
2021-12-16 15:07:49 -0500

    Added dataframes

--- streamlit_final.py
+++ streamlit_final.py
@@ -9,5 +9,30 @@ Created on Thu Dec 16 14:19:21 2021
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
 
-st.title('Uber pickups in NYC')
+
+@st.cache
+def load_hospitals():
+    df_hospital_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
+    return df_hospital_2
+
+@st.cache
+def load_inatpatient():
+    df_inpatient_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient_2.csv')
+    return df_inpatient_2
+
+@st.cache
+def load_outpatient():
+    df_outpatient_2 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
+    return df_outpatient_2
+
+
+st.title('Medicare — Expenses - NY / NY State')
+
+##How does Stony Brook compare to the rest of NY?
+##sTONY Brook - Most expensive inpatient DRGs
+##Stony Brook - Most expensive outpatient DRGs
