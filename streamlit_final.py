4cc5b35341b25a4d031eefbcdb2fc931f3defea6
Alvin <alvin.devasia@stonybrook.edu>
2021-12-16 16:21:44 -0500

    quick edit

--- streamlit_final.py
+++ streamlit_final.py
@@ -16,3 +16,16 @@ st.title('Medicare — Expenses - National')
 ##How does Stony Brook compare to the rest of NY?
 ##sTONY Brook - Most expensive inpatient DRGs
 ##Stony Brook - Most expensive outpatient DRGs
+
+@st.cache
+def load_hospitals():
+   hospitaldf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
+   return hospitaldf
+@st.cache
+def load_inatpatient():
+    inpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
+    return inpatientdf
+@st.cache
+def load_outpatient():
+    outpatientdf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
+    return outpatientdf
