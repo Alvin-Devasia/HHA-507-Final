da03ddbd1b14c6f04507ad8a96f27e5400412cea
Alvin <alvin.devasia@stonybrook.edu>
2021-12-16 16:19:36 -0500

    Quick edit

--- streamlit_final.py
+++ streamlit_final.py
@@ -9,29 +9,6 @@ Created on Thu Dec 16 14:19:21 2021
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import time
-import matplotlib.pyplot as plt
-
-
-@st.cache
-def load_hospitals():
-    hospital_info = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
-    return hospital_info
-
-@st.cache
-def load_outpatient():
-    outpatient2015 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
-    return outpatient2015
-
-@st.cache
-def load_inpatient():
-    inpatient2015 = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient_2.csv')
-    return inpatient2015
-
-
 
 
 st.title('Medicare — Expenses - National')
