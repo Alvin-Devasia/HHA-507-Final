962ebfa4a8c1591886d6b89a61aa7bf03b72e675
Alvin <alvin.devasia@stonybrook.edu>
2021-12-16 14:21:12 -0500

    Added streamlit_final

---
+++ streamlit_final.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 16 14:19:21 2021
+
+@author: AlvinDevasia
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
