4cf8e762a89e4417777009ee21a80d08b7ba3f0a
Alvin <alvin.devasia@stonybrook.edu>
2021-12-16 20:52:20 -0500

    quick edit

--- streamlit_final.py
+++ streamlit_final.py
@@ -6,10 +6,15 @@ Created on Thu Dec 16 14:19:21 2021
 @author: AlvinDevasia
 """
 
-import streamlit as st
 import pandas as pd
+import plotly.express as px
+import pandas as pd
+import streamlit as st
 import numpy as np
-
+import time
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly
 
 
 ##How does Stony Brook compare to the rest of NY?
@@ -66,6 +71,24 @@ st.title('Inpatient and outpatient dataframes')
 st.subheader('Inpatient Facility')
 bar7 = inpatientdf['provider_state'].value_counts().reset_index()
 st.dataframe(bar7)
+
+st.subheader('Bar Chart of Inpatient Facilities by state')
+fig7 = px.bar(bar7, x='index', y='provider_state')
+st.plotly_chart(fig7)
+
+
+st.markdown('The dataframe displayed below is for the outpatient facility')
+
+st.subheader('Outpatient Facility')
+bar7 = outpatientdf['provider_state'].value_counts().reset_index()
+st.dataframe(bar7)
+
+st.subheader('Bar Chart of outpatient Facilities by state')
+fig7 = px.bar(bar7, x='index', y='provider_state')
+st.plotly_chart(fig7)
+
+st.markdown('2.  Which states have the greatest number of inpatient and outpatient facilities?')
+st.markdown('- As shown by the analysis above, Florida has the most inpatient facilities and Texas has the most outpatient facilities') 
 
 
 
